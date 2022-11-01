0ad428c0126cc39d1f519cff9064ccd3c371e1a1
Oghenekparobo <classicsteph11@gmail.com>
2022-11-01 07:07:05 +0100

    learnt how t fetch api with react

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,34 @@
+import { useEffect, useState } from "react";
+import { Reorder } from "framer-motion";
 
-import { useEffect ,  useState  } from "react";
 import Row from "./components/Row";
 function App() {
-  const [cryptoData , setCryptoData] = useState();
-  
-  useEffect(()=> {
-    const fetchCryptoData = async () =>{ 
-      const data = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=ngn');
-      const apiResponse = await data.json();
-      console.log( apiResponse );
-      setCryptoData([apiResponse]);
-    }
-  
-    fetchCryptoData();
-  }, [cryptoData ])
+  const [cryptoData, setCryptoData] = useState([]);
 
-  
+  useEffect(() => {
+    const fetchCryptoData = async () => {
+      const data = await fetch(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd"
+      );
+      const apiResponse = await data.json();
+      const sortedData = apiResponse.sort(
+        (a, b) => b.price_change_percentage_24h - a.price_change_percentage_24h
+      );
+      // console.log( apiResponse );
+      setCryptoData(sortedData);
+    };
+
+    const interval = setInterval(() => {
+      fetchCryptoData();
+      console.log("real time apiResponse ");
+    }, 4000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [cryptoData]);
+
   return (
-    <div className="table">
+    <div className="flex justify-center">
       <div className="container mx-auto px-4 sm:px-8">
         <div className="py-8">
           <div>
@@ -27,40 +38,40 @@ function App() {
           </div>
           <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
             <div className="inline-block min-w-full shadow-md rounded-lg overflow-hidden">
-              <table className="min-w-full leading-normal">
-                <thead>
-                  <tr>
-                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
-                      Cryptocurrency
-                    </th>
-                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
-                      Price
-                    </th>
-                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
-                      Market Cap
-                    </th>
-                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
-                      Total Volume
-                    </th>
-                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
-                      Price Change 24h
-                    </th>
-                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
-                      Price Percentage Change 24h
-                    </th>
-                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
-                      Price Trend
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-
-                  {cryptoData.map((data)=>(<Row key={data.id} crypto={data}/>))}
-             
-               
-               
-                </tbody>
-              </table>
+              <Reorder.Group values={cryptoData} onReorder={setCryptoData}>
+                <table className="min-w-full leading-normal">
+                  <thead>
+                    <tr>
+                      <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+                        Cryptocurrency
+                      </th>
+                      <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+                        Price
+                      </th>
+                      <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+                        Market Cap
+                      </th>
+                      <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+                        Total Volume
+                      </th>
+                      <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+                        Price Change 24h
+                      </th>
+                      <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+                        Price Percentage Change 24h
+                      </th>
+                      <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+                        Price Trend
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {cryptoData.map((data) => (
+                      <Row key={data.id} crypto={data} />
+                    ))}
+                  </tbody>
+                </table>
+              </Reorder.Group>
             </div>
           </div>
         </div>
--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,55 +1,68 @@
-import React from 'react'
+import { Reorder } from "framer-motion";
 
-export default function Row({crypto}) {
+export default function Row({ crypto }) {
   return (
-    
-    <tr>
-    <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-      <div className="flex">
-        <div className="flex-shrink-0 w-10 h-10">
-          <img
-            className="w-full h-full rounded-full"
-            src={crypto.image}
-            alt="crypto banner"
-          />
+    <Reorder.Item as="tr" key={crypto.price_change_percentage_24h} value={crypto.price_change_percentage_24h}>
+      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+        <div className="flex">
+          <div className="flex-shrink-0 w-10 h-10">
+            <img
+              className="w-full h-full rounded-full"
+              src={crypto.image}
+              alt="crypto banner"
+            />
+          </div>
+          <div className="ml-3">
+            <p className="text-gray-900 whitespace-no-wrap">{crypto.name}</p>
+            <p className="text-gray-600 whitespace-no-wrap">{crypto.symbol}</p>
+          </div>
         </div>
-        <div className="ml-3">
-          <p className="text-gray-900 whitespace-no-wrap">
-          {crypto.name}
-          </p>
-          <p className="text-gray-600 whitespace-no-wrap">{crypto.symbol}</p>
-        </div>
-      </div>
-    </td>
-    <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-      <p className="text-gray-900 whitespace-no-wrap">${crypto.current_price}</p>
-      <p className="text-gray-600 whitespace-no-wrap">USD</p>
-    </td>
-    <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-      <p className="text-gray-600 whitespace-no-wrap">${crypto.market_cap}</p>
-    </td>
-    <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-      <p className="text-gray-600 whitespace-no-wrap">${crypto.total_volume}</p>
-    </td>
-    <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-      <p className="text-gray-600 whitespace-no-wrap">${crypto.price_change_24h}</p>
-    </td>
-    <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-     
-     <p className="text-gray-600 whitespace-no-wrap">
-     ${crypto.price_change_percentage_24h}
-     </p>
-   </td>
-    <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-      <span className="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
-        <span
-          aria-hidden
-          className="absolute inset-0 bg-green-200 opacity-50 rounded-full"
-        ></span>
-        <span className="relative">Upward Trend</span>
-      </span>
-    </td>
-  </tr>
- 
-  )
+      </td>
+      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+        <p className="text-gray-900 whitespace-no-wrap">
+          ${crypto.current_price}
+        </p>
+        <p className="text-gray-600 whitespace-no-wrap">USD</p>
+      </td>
+      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+        <p className="text-gray-600 whitespace-no-wrap">${crypto.market_cap}</p>
+      </td>
+      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+        <p className="text-gray-600 whitespace-no-wrap">
+          ${crypto.total_volume}
+        </p>
+      </td>
+      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+        <p className="text-gray-600 whitespace-no-wrap">
+          ${crypto.price_change_24h}
+        </p>
+      </td>
+      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+        <p className="text-gray-600 whitespace-no-wrap">
+          {crypto.price_change_percentage_24h}%
+        </p>
+      </td>
+      {crypto.price_change_percentage_24h > 0 ? (
+        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+          <span className="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
+            <span
+              aria-hidden
+              className="absolute inset-0 bg-green-200 opacity-50 rounded-full"
+            ></span>
+            <span className="relative">Upward Trend</span>
+          </span>
+        </td>
+      ) : (
+        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+          <span className="relative inline-block px-3 py-1 font-semibold text-red-900 leading-tight">
+            <span
+              aria-hidden
+              className="absolute inset-0 bg-red-200 opacity-50 rounded-full"
+            ></span>
+            <span className="relative">Downward Trend</span>
+          </span>
+        </td>
+      )}
+   </Reorder.Item>
+  );
 }
